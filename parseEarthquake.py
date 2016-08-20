0014c117b39b3f3d80552caa73cdb55d75a6bb72
Davinder Mahal <davinder@mahal.org>
2016-08-19 18:33:35 -0700

    Writes to DB and update gitignore

--- parseEarthquake.py
+++ parseEarthquake.py
@@ -1,37 +1,92 @@
 #!/usr/bin/env python
 
-import urllib3, json
+import urllib3
+import json
+import datetime
+from datetime import date
+import sqlite3
+
+
+class USGSFeed:
+
+    _feedDuration = ('hour', 'day', 'week', 'month')
+
+    _feedSize = ('significant', '4.5', '2.5', '1.0', 'all')
+
+    _feedUrl = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/{}_{}.geojson'
+
+    def get(self, duration, size='all'):
+        if (duration in self._feedDuration) & (size in self._feedSize):
+            return self._feedUrl.format(size, duration)
+        else:
+            raise LookupError
+
 
 
 class Earthquake:
     def __init__(self, item):
-        self.id = item['id']
-        self.properties = item['properties']
-        self.geometry = item['geometry']
-        self.type = item['type']
-        self.title = self.properties['title']
-        self.magnitude = self.properties['mag']
-        self.time = self.properties['time']
+        self._vars = {
+            'id':item['id'],
+            'geometry': item['geometry'],
+            'properties': item['properties'],
+            'raw': item,
+        }
+
+    def get_properties(self):
+        return self._vars['properties']
+
+    def get_property(self, name):
+        try:
+            return self._vars['properties'][name]
+        except:
+            print("Key", name, "does not exist.")
+
+    def get_attribute(self, name):
+        return self._vars[name]
+
+    def get_geometry(self):
+        return self._vars['geometry']
+
+    def get_raw(self):
+        return self._vars['raw']
 
 
 
 def main():
     http = urllib3.PoolManager()
-    hourlyUSGSFeed = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
-    response = http.request('GET', hourlyUSGSFeed)
+    usgs = USGSFeed()
+    feed = usgs.get('month', 'all');
+    response = http.request('GET', feed)
 
     if (response.status == 200):
+        db = sqlite3.connect('earthquakes.db')
+        cursor = db.cursor()
+        cursor.execute(''' DROP TABLE earthquakes ''')
+        cursor.execute('''
+            CREATE TABLE earthquakes(id INTEGER PRIMARY KEY, earthquake_id TEXT, title TEXT, 
+            magnitude REAL, url TEXT, data TEXT)''')
+
         earthquakes = []
-
-        decodedJsonData = json.loads(response.data.decode('utf-8'))
-        for key, earthquakesData in decodedJsonData.items():
+        database_insert = []
+        decoded_json_data = json.loads(response.data.decode('utf-8'))
+        for key, earthquakes_data in decoded_json_data.items():
             if key == 'features':
-                for earthquakeData in earthquakesData:
-                    earthquake = Earthquake(earthquakeData)
-                    earthquakes.append(earthquake)
-                    #print(earthquake.properties)
-        print(earthquakes)
+                for earthquake_data in earthquakes_data:
 
+                    earthquake = Earthquake(earthquake_data)
+                    earthquakes.append(earthquake)
+
+                    new_row = (earthquake.get_attribute('id'), 
+                        earthquake.get_property('title'), 
+                        earthquake.get_property('mag'),  
+                        earthquake.get_property('url'), 
+                    )
+
+                    database_insert.append(new_row)
+
+        print("Adding to database")
+        cursor.executemany(''' INSERT INTO earthquakes(earthquake_id, title, magnitude, url) values(?,?,?,?)''', database_insert)
+        db.commit()
     else:
         print("Error: Request to", hourlyUSGSFeed, " Status:", response.status)
 
