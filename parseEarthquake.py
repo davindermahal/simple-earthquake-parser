23d3d88ed8ce446997b19b918cd13963d63153c4
Davinder Mahal <davinder@mahal.org>
2016-08-23 15:19:50 -0700

    Add more columns

--- parseEarthquake.py
+++ parseEarthquake.py
@@ -38,7 +38,7 @@ class Earthquake:
     def get_property(self, name):
         try:
             return self._vars['properties'][name]
-        except:
+        except BaseException:
             print("Key", name, "does not exist.")
 
     def get_attribute(self, name):
@@ -55,37 +55,59 @@ class Earthquake:
 def main():
     http = urllib3.PoolManager()
     usgs = USGSFeed()
-    feed = usgs.get('month', 'all');
+    feed = usgs.get('day', 'all');
     response = http.request('GET', feed)
 
-    if (response.status == 200):
+    if response.status == 200:
         db = sqlite3.connect('earthquakes.db')
         cursor = db.cursor()
         cursor.execute(''' DROP TABLE earthquakes ''')
         cursor.execute('''
-            CREATE TABLE earthquakes(id INTEGER PRIMARY KEY, earthquake_id TEXT, title TEXT, 
-            magnitude REAL, url TEXT, data TEXT)''')
+            CREATE TABLE earthquakes(id INTEGER PRIMARY KEY, earthquake_id TEXT UNIQUE, title TEXT,
+            magnitude REAL, type TEXT, url TEXT, data TEXT, created_at INTEGER)''')
 
         earthquakes = []
         database_insert = []
         decoded_json_data = json.loads(response.data.decode('utf-8'))
+
+
+        #set up the metadata table
+        cursor.execute(''' DROP TABLE metadata ''')
+        cursor.execute('''
+            CREATE TABLE metadata(id INTEGER PRIMARY KEY, generated TEXT UNIQUE, created_at INTEGER)''')
+        metadata = json.dumps(decoded_json_data['metadata']['generated'])
+
+        cursor.execute(''' INSERT INTO metadata(generated, created_at) values (?,?)''', (metadata,datetime.datetime.utcnow()))
+        # print(decoded_json_data)
+        ''' look at the rest of the data feed.
+        I would need to store that.
+        How do I convert the Dict back to a string so it can be inserted into the database
+        '''
+
         for key, earthquakes_data in decoded_json_data.items():
             if key == 'features':
                 for earthquake_data in earthquakes_data:
 
                     earthquake = Earthquake(earthquake_data)
                     earthquakes.append(earthquake)
+                    raw_data = json.dumps(earthquake.get_raw())
+                    #print(raw_data)
 
                     new_row = (earthquake.get_attribute('id'), 
                         earthquake.get_property('title'), 
-                        earthquake.get_property('mag'),  
-                        earthquake.get_property('url'), 
+                        earthquake.get_property('mag'),
+                        earthquake.get_property('type'),
+                        earthquake.get_property('url'),
+                        raw_data,
+                        datetime.datetime.utcnow(),
                     )
 
                     database_insert.append(new_row)
 
         print("Adding to database")
-        cursor.executemany(''' INSERT INTO earthquakes(earthquake_id, title, magnitude, url) values(?,?,?,?)''', database_insert)
+        cursor.executemany(''' INSERT INTO earthquakes(
+            earthquake_id, title, magnitude, type, url, data, created_at)
+        values(?,?,?,?,?,?,?)''', database_insert)
         db.commit()
     else:
         print("Error: Request to", hourlyUSGSFeed, " Status:", response.status)
